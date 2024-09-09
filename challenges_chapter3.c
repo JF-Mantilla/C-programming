2c22b813bd34c9bd79edd62c184d56e6e6fcaa71
Felipe Mantilla <jf.mantilla77@gmail.com>
2024-09-09 14:31:21 -0400

    Project Exercise 1 and 2 added

--- challenges_chapter3.c
+++ challenges_chapter3.c
@@ -17,9 +17,35 @@ void exe1(){
 
 }
 
+void exe2(){
+    /* format product information entered by user */
+    int day, year, month, item;
+    float price;
+
+    printf("Enter a date (mm/dd/yyyy): ");
+    scanf("%d/%d/%d", &month, &day, &year);
+
+    // Output the date in yyyymmdd format
+    printf("Enter # of item: ");
+    scanf("%d",&item);
+
+    printf("Enter unit price: ");
+    scanf("%f", &price);
+
+        // Print formatted output
+    printf("\nItem\t\tUnit\t\tPurchase\n");
+    printf("\t\tPrice\t\tDate\n");
+
+    // Left justify item and date, right justify unit price
+    printf("%-d\t\t$%7.2f\t%02d/%02d/%d\n", item, price, month, day, year);
+
+}
+
+
 
 int main(){
-    exe1();
+    //exe1();
+    exe2();
 
     return 0;
 }
