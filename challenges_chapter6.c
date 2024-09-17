57d9d2492cd328278f69b9e49a94c3d4f89e090b
Felipe Mantilla <jf.mantilla77@gmail.com>
2024-09-17 12:09:30 -0400

    Updated Chapter 6 challenges

--- challenges_chapter6.c
+++ challenges_chapter6.c
@@ -77,6 +77,90 @@ int exe3(){
     reduce_fraction(numerator, denominator);
 }
 
+int exe4(){
+
+    int num, temp=0, op;
+    printf("Enter a number ");
+    scanf("%d",&num);
+
+    do
+    {
+        op = num % 10;
+        temp = temp * 10 + op;
+        num /= 10;
+    } while (num != 0);
+
+
+    
+    printf ("The reverse number is %d", temp);
+
+
+
+}
+
+int exe5(){
+    int days, start;
+
+    printf("Enter number of days in month: ");
+    scanf("%d", &days);
+    printf(" Enter starting day of the week (1=Sun, 7=Sat): ");
+    scanf("%d", &start);
+
+    // Print the header for the calendar
+    printf("\n Sun Mon Tue Wed Thu Fri Sat\n");
+
+        // Print leading spaces for the first week
+    for (int i = 1; i < start; i++) {
+        printf("    "); // Four spaces for each empty day
+    }
+
+    // Print the days of the month
+    for (int day = 1; day <= days; day++) {
+        printf("%4d", day); // Print the day with a width of 4 for proper alignment
+
+        // Move to the next line after every Saturday (7th day of the week)
+        if ((day + start - 1) % 7 == 0) {
+            printf("\n");
+        }
+    }
+
+    printf("\n");
+    
+}
+
+// Function to calculate factorial of a number
+long long factorial(int num) {
+    long long fact = 1;
+    for (int i = 1; i <= num; i++) {
+        fact *= i;
+    }
+    return fact;
+}
+
+int exe6(){
+
+    double epsilon;
+    double e = 1.0; // Start with 1 (corresponding to 1/0!)
+    int i = 1;      // Counter for the series terms
+    double term;
+
+    // Get user input for the value of epsilon
+    printf("Enter a small number epsilon: ");
+    scanf("%lf", &epsilon);
+
+    // Calculate the series until the current term is less than epsilon
+    do {
+        term = 1.0 / factorial(i);  // Calculate current term
+        e += term;                  // Add current term to the value of e
+        i++;                        // Move to the next term in the series
+    } while (term >= epsilon);      // Continue while the term is greater than or equal to epsilon
+
+    // Print the result
+    printf("The approximation of e is: %lf\n", e);
+
+    return 0;
+}
+
 int main(void){
 
 int opcion;
@@ -87,8 +171,9 @@ int opcion;
         printf("1. Largest number \n");
         printf("2. Greates common divisor \n");
         printf("3. reduce fraction \n");
-        printf("4. largest / smallest \n");
-        printf("5. date earlier \n");
+        printf("4. reverse number \n");
+        printf("5. calendar \n");
+        printf("6. aproximate e (euler) \n");
         printf("Enter option (1-5): \n");
         printf("Enter 7 to exit: ");
         scanf("%d", &opcion);
@@ -99,19 +184,23 @@ int opcion;
                 break;
             
             case 2:
-                //exe2();
+                exe2();
                 break;
 
             case 3:
-                //exe3();
+                exe3();
                 break;
 
             case 4:
-                //exe4();
+                exe4();
                 break;
 
             case 5:
-                //exe5();
+                exe5();
+                break;
+
+            case 6:
+                exe6();
                 break;
 
             default:
